3f64f31822fc2013cf7cf06ccb9b7b60bb1c5e77
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-03-04 22:03:03 +0100

    feat: Add bootstrap's JS

--- assets/index.ts
+++ assets/index.ts
@@ -2,4 +2,7 @@
 /// <reference lib="dom" />
 //@ts-ignore
 /// <reference lib="deno.ns" />
+// TODO do not import everything - probably not possible with bootstrap's current architecture
+import './node_modules/bootstrap/dist/js/bootstrap.js';
+
 console.log(document);
