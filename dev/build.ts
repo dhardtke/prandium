8e20da14838040949ec6dde58f5dbb7a36529099
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-03-04 22:03:12 +0100

    feat: Improve build.ts

--- dev/build.ts
+++ dev/build.ts
@@ -9,6 +9,7 @@ interface Step {
     fn: () => Promise<any>;
 }
 
+// TODO build index.min.css and leave index.css unminified
 const steps: Step[] = [
     {
         description: "Compile SCSS to CSS",
@@ -18,21 +19,26 @@ const steps: Step[] = [
         description: "Compile TS to JS",
         fn: processAsync("assets", "deno", "bundle", "index.ts", "dist/index.js")
     },
+    {
+        description: "Minify JS",
+        fn: processAsync("assets", "esbuild", "--minify", "dist/index.js", "--outfile=dist/index.min.js")
+    },
     // {
     //     description: "Compile server",
     //     fn: processAsync("", "") // TODO
     // },
     {
         description: "Compress assets (using gzip)",
-        fn: processAsync("assets/dist", "gzip", "-f", "-k", "index.css", "index.js")
+        fn: processAsync("assets/dist", "gzip", "-f", "-k", "index.css", "index.js", "index.min.js")
     },
     {
         description: "Compress assets (using brotli)",
-        fn: processAsync("assets/dist", "brotli", "-f", "-k", "index.css", "index.js")
+        fn: processAsync("assets/dist", "brotli", "-f", "-k", "index.css", "index.js", "index.min.js")
     }
 ];
 
 if (import.meta.main) {
+    // TODO clean output dir
     for (const step of steps) {
         const prefix = Colors.blue(`[${step.description}]`);
         console.log(`${prefix} Start`);
