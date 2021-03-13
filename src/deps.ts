fd3cc9a2827ff8ba007d5b44707864ca0d3d3be4
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-03-13 15:01:14 +0100

    feat: Update std@0.89.0 -> std@0.90.0

--- src/deps.ts
+++ src/deps.ts
@@ -4,11 +4,11 @@ export * as Dom from "https://deno.land/x/deno_dom@v0.1.6-alpha/deno-dom-wasm.ts
 export * as Oak from "https://deno.land/x/oak@v6.5.0/mod.ts";
 
 // utils
-export * as path from "https://deno.land/std@0.89.0/path/mod.ts";
-export * as log from "https://deno.land/std@0.89.0/log/mod.ts";
-export {LogRecord} from "https://deno.land/std@0.89.0/log/logger.ts";
+export * as path from "https://deno.land/std@0.90.0/path/mod.ts";
+export * as log from "https://deno.land/std@0.90.0/log/mod.ts";
+export {LogRecord} from "https://deno.land/std@0.90.0/log/logger.ts";
 export * as Cliffy from "https://deno.land/x/cliffy@v0.17.2/command/mod.ts";
-export * as Colors from "https://deno.land/std@0.89.0/fmt/colors.ts";
+export * as Colors from "https://deno.land/std@0.90.0/fmt/colors.ts";
 
 // types
 export type {Recipe} from "https://cdn.skypack.dev/schema-dts@v0.8.2/schema.d.ts";
