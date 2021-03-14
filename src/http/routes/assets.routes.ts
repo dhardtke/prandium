c75c7796dfd98a6fe287066f29ef317378a1029b
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-03-14 04:30:43 +0100

    feat: Add favicons

--- src/http/routes/assets.routes.ts
+++ src/http/routes/assets.routes.ts
@@ -1,13 +1,16 @@
-import { Oak, path } from "../../deps.ts";
+import { log, Oak } from "../../deps.ts";
+import { root } from "../../util.ts";
 
-const COMPILED_ASSETS_DIR = path.resolve(Deno.cwd(), "assets", "dist");
+const COMPILED_ASSETS_DIR = root("assets");
 
 const router: Oak.Router = new Oak.Router();
-router.get("/assets/:name", async (ctx) => {
-  if (ctx.params?.name) {
-    await Oak.send(ctx, ctx.params.name, {
+router.get("/assets/(.+)", async (ctx, next) => {
+  try {
+    await Oak.send(ctx, ctx.params[0]!, {
       root: COMPILED_ASSETS_DIR,
     });
+  } catch {
+    await next();
   }
 });
 
