c8e73e565805bf7f532b807205af317e36199c27
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-03-21 03:06:44 +0100

    feat: Include minified files if they're available

---
+++ src/tpl/helpers/assets_helper.ts
@@ -0,0 +1,21 @@
+import { fs, path } from "../../deps.ts";
+import { root } from "../../util.ts";
+
+export class AssetsHelper {
+  public static INSTANCE: AssetsHelper = new AssetsHelper();
+
+  private constructor() {
+  }
+
+  public ifExists = (checked: string, _default: string): string => {
+    const COMPILED_ASSETS_DIR = root("assets", "dist");
+    if (fs.existsSync(path.join(COMPILED_ASSETS_DIR, checked))) {
+      return checked;
+    }
+    return _default;
+  };
+
+  public api = {
+    ifExists: this.ifExists,
+  };
+}
--- src/tpl/helpers/helpers.ts
+++ src/tpl/helpers/helpers.ts
@@ -1,10 +1,12 @@
 import { UrlHelper } from "../../http/url_helper.ts";
+import { AssetsHelper } from "./assets_helper.ts";
 import { DateHelper } from "./date_helper.ts";
 import { IconHelper } from "./icon_helper.ts";
 import { IngredientHelper } from "./ingredient_helper.ts";
 import { TranslationHelper } from "./translation_helper.ts";
 
 export const Helpers = {
+  ...AssetsHelper.INSTANCE.api,
   ...TranslationHelper.INSTANCE.api,
   ...IconHelper.INSTANCE.api,
   ...IngredientHelper.INSTANCE.api,
