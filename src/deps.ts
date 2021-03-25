a148172fb9f4a7848751b9b22a8752835bed97d9
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-03-25 22:06:21 +0100

    feat: Update to sqlite@2.4.0

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,4 @@
-export * as sqlite from "https://deno.land/x/sqlite@v2.3.2/mod.ts";
+export * as sqlite from "https://deno.land/x/sqlite@v2.4.0/mod.ts";
 export * as Eta from "https://deno.land/x/eta@v1.12.1/mod.ts";
 export * as Dom from "https://deno.land/x/deno_dom@v0.1.7-alpha/deno-dom-wasm.ts";
 export * as Oak from "https://deno.land/x/oak@v6.5.0/mod.ts";
