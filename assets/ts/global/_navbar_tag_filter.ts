e09655f19711afdaa2cd7629fa9ceaaf53066815
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-03-28 20:16:18 +0200

    feat: Do not reset tag list, only update changes

--- assets/ts/global/_navbar_tag_filter.ts
+++ assets/ts/global/_navbar_tag_filter.ts
@@ -75,6 +75,26 @@ export class NavbarTagFilter {
     }
   };
 
+  private addItemClickListener($item: HTMLAnchorElement) {
+    $item.addEventListener("click", (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      if (parseInt($item.querySelector(SELECTORS.RECIPE_COUNT)!.textContent!, 10) === 0) {
+        return;
+      }
+      const id = parseInt($item.dataset.tagId!, 10);
+      const isActive = $item.classList.toggle(CLASSES.ACTIVE);
+      const $input = $item.querySelector("input") as HTMLInputElement;
+      $input.disabled = !isActive;
+      if (isActive) {
+        this.activeTagIds.add(id);
+      } else {
+        this.activeTagIds.delete(id);
+      }
+      this.load();
+    });
+  };
+
   private buildTagUrl(tagId: number): string {
     const url = new URL(this.requestUrl.toString());
     if (this.activeTagIds.has(tagId)) {
@@ -91,50 +111,35 @@ export class NavbarTagFilter {
     return url;
   }
 
-  private buildItem(tag: Tag, $fragment: DocumentFragment) {
-    const $input = $fragment.querySelector("input") as HTMLInputElement;
+  private updateItem(tag: Tag, $item: HTMLAnchorElement) {
+    const $input = $item.querySelector("input") as HTMLInputElement;
     $input.value = tag.id + "";
-    const $item = $fragment.querySelector("a") as HTMLAnchorElement;
     $item.dataset.tagId = tag.id + "";
     $item.querySelector(SELECTORS.TITLE)!.textContent = tag.title;
     $item.querySelector(SELECTORS.RECIPE_COUNT)!.textContent = tag.recipeCount + "";
     $item.href = this.buildTagUrl(tag.id);
-    $item.addEventListener("click", async (e) => {
-      e.preventDefault();
-      e.stopPropagation();
-      if (tag.recipeCount === 0) {
-        return;
-      }
-      const isActive = $item.classList.toggle(CLASSES.ACTIVE);
-      $input.disabled = !isActive;
-      if (isActive) {
-        this.activeTagIds.add(tag.id);
-      } else {
-        this.activeTagIds.delete(tag.id);
-      }
-      this.load();
-    });
     $input.disabled = true;
     if (this.activeTagIds.has(tag.id)) {
       $item.classList.add(CLASSES.ACTIVE);
       $input.disabled = false;
-    } else if (tag.recipeCount === 0) {
-      $item.classList.add(CLASSES.DISABLED);
     }
-    if (this.hiddenTagIds.has(tag.id)) {
-      $item.classList.add(CLASSES.HIDDEN);
-    }
+    $item.classList.toggle(CLASSES.DISABLED, tag.recipeCount === 0);
+    $item.classList.toggle(CLASSES.HIDDEN, this.hiddenTagIds.has(tag.id));
   }
 
   private load() {
     // @ts-ignore
     fetch(this.buildEndpointUrl()).then(async (response) => {
-      this.$results.innerHTML = "";
       const tags: Tag[] = await response.json();
       for (const tag of tags) {
-        const $fragment = this.$template.content.cloneNode(true) as DocumentFragment;
-        this.buildItem(tag, $fragment);
-        this.$results.appendChild($fragment);
+        let $item = this.$results.querySelector(`[data-tag-id="${tag.id}"]`) as HTMLAnchorElement;
+        if (!$item) {
+          const $fragment = this.$template.content.cloneNode(true) as DocumentFragment;
+          $item = $fragment.querySelector("a") as HTMLAnchorElement;
+          this.addItemClickListener($item);
+          this.$results.appendChild($fragment);
+        }
+        this.updateItem(tag, $item);
       }
     });
   }
