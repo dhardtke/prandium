1e3285a9b6d3939049add3d421c191820a7f7118
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-04-01 22:33:54 +0200

    chore: Update to latest cliffy

--- dev/internal/deps.ts
+++ dev/internal/deps.ts
@@ -3,4 +3,4 @@ export * as path from "https://deno.land/std@0.91.0/path/mod.ts";
 export * as fs from "https://deno.land/std@0.91.0/fs/mod.ts";
 export { existsSync } from "https://deno.land/std@0.91.0/fs/mod.ts";
 export * as Colors from "https://deno.land/std@0.91.0/fmt/colors.ts";
-export * as Cliffy from "https://deno.land/x/cliffy@v0.17.2/command/mod.ts";
+export * as Cliffy from "https://deno.land/x/cliffy@v0.18.1/command/mod.ts";
--- src/deps.ts
+++ src/deps.ts
@@ -23,7 +23,7 @@ export const dateFns = {
 export * as path from "https://deno.land/std@0.91.0/path/mod.ts";
 export * as log from "https://deno.land/std@0.91.0/log/mod.ts";
 export { LogRecord } from "https://deno.land/std@0.91.0/log/logger.ts";
-export * as Cliffy from "https://deno.land/x/cliffy@v0.17.2/command/mod.ts";
+export * as Cliffy from "https://deno.land/x/cliffy@v0.18.1/command/mod.ts";
 export * as fs from "https://deno.land/std@0.91.0/fs/mod.ts";
 export * as Colors from "https://deno.land/std@0.91.0/fmt/colors.ts";
 
