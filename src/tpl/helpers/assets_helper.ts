ada429024e5ae2b04dec4a13906112f7e906428e
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-04-02 20:17:10 +0200

    feat: Add cache busting for FE assets

--- src/tpl/helpers/assets_helper.ts
+++ src/tpl/helpers/assets_helper.ts
@@ -1,6 +1,8 @@
 import { fs, path } from "../../../deps.ts";
 import { root } from "../../util.ts";
 
+const COMPILED_ASSETS_DIR = root("assets", "dist");
+
 export class AssetsHelper {
   public static INSTANCE: AssetsHelper = new AssetsHelper();
 
@@ -8,14 +10,19 @@ export class AssetsHelper {
   }
 
   public ifExists = (checked: string, _default: string): string => {
-    const COMPILED_ASSETS_DIR = root("assets", "dist");
     if (fs.existsSync(path.join(COMPILED_ASSETS_DIR, checked))) {
       return checked;
     }
     return _default;
   };
 
+  public modificationTimestamp = (filename: string): number | undefined => {
+    return Deno.statSync(path.join(COMPILED_ASSETS_DIR, filename)).mtime
+      ?.getTime();
+  };
+
   public api = {
     ifExists: this.ifExists,
+    modificationTimestamp: this.modificationTimestamp,
   };
 }
