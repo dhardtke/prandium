57f5cdf99ff0671da86f6ef2e46850ee027ddc98
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-04-11 18:31:57 +0200

    feat: Use wake-lock API if supported (#22)

--- assets/index.ts
+++ assets/index.ts
@@ -8,7 +8,7 @@ import "./deps.ts";
 import { DarkModeSwitcher } from "./ts/global/_navbar_dark_mode_switcher.ts";
 import { removeUrlFlashParameter } from "./ts/global/_remove_url_flash_parameter.ts";
 import { showDocument } from "./ts/global/_show_document.ts";
-import { RecipeDetailPage } from "./ts/page/recipe_detail_page.ts";
+import { RecipeDetailPage } from "./ts/page/recipe_detail_page/recipe_detail_page.ts";
 import { RecipeEditPage } from "./ts/page/recipe_edit_page/recipe_edit_page.ts";
 import { RecipeListPage } from "./ts/page/recipe_list_page/recipe_list_page.ts";
 
@@ -40,8 +40,13 @@ const routes = [
   }
 ];
 
-for (const route of routes) {
-  if (window.location.pathname.match(route.match)) {
-    route.fn();
+(async () => {
+  for (const route of routes) {
+    if (window.location.pathname.match(route.match)) {
+      const result = route.fn();
+      if (result as unknown instanceof Promise) {
+        await result;
+      }
+    }
   }
-}
+})();
