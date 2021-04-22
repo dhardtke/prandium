1c44aeb4fc3ba9deb68f2751b828e5917504474b
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-04-22 20:47:40 +0200

    chore: Update deps

--- deps.ts
+++ deps.ts
@@ -1,7 +1,6 @@
 export * as sqlite from "https://deno.land/x/sqlite@v2.4.0/mod.ts";
-export * as Dom from "https://deno.land/x/deno_dom@v0.1.7-alpha/deno-dom-wasm.ts";
+export * as Dom from "https://deno.land/x/deno_dom@v0.1.8-alpha/deno-dom-wasm.ts";
 export * as Oak from "https://deno.land/x/oak@v7.3.0/mod.ts";
-export { HttpServerStd } from "https://deno.land/x/oak@v7.3.0/http_server_std.ts";
 export { default as parseIngredient } from "https://cdn.skypack.dev/parse-ingredient@v0.3.0?min";
 export * from "https://cdn.skypack.dev/numeric-quantity@v1.0.1?min";
 export * as Zod from "https://cdn.skypack.dev/zod@3.0.0-alpha.33?dts";
@@ -30,7 +29,7 @@ export const dateFns = {
 export * as path from "https://deno.land/std@0.94.0/path/mod.ts";
 export * as log from "https://deno.land/std@0.94.0/log/mod.ts";
 export { LogRecord } from "https://deno.land/std@0.94.0/log/logger.ts";
-export * as Cliffy from "https://deno.land/x/cliffy@v0.18.1/command/mod.ts";
+export * as Cliffy from "https://deno.land/x/cliffy@v0.18.2/command/mod.ts";
 export * as fs from "https://deno.land/std@0.94.0/fs/mod.ts";
 export * as Colors from "https://deno.land/std@0.94.0/fmt/colors.ts";
 export { default as slash } from "https://deno.land/x/slash@v0.3.0/mod.ts";
