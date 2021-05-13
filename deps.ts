a495c6d8febc5d66e95b9af36fb6f20b28cf7e5d
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-05-14 00:18:33 +0200

    feat: Update to Deno 1.10.1 and update deps

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 export * as sqlite from "https://deno.land/x/sqlite@v2.4.0/mod.ts";
 export * as Dom from "https://deno.land/x/deno_dom@v0.1.8-alpha/deno-dom-wasm.ts";
-export * as Oak from "https://deno.land/x/oak@v7.3.0/mod.ts";
-export { HttpServerStd } from "https://deno.land/x/oak@v7.3.0/http_server_std.ts";
+export * as Oak from "https://deno.land/x/oak@v7.4.1/mod.ts";
+export { HttpServerStd } from "https://deno.land/x/oak@v7.4.1/http_server_std.ts";
 export { default as parseIngredient } from "https://cdn.skypack.dev/parse-ingredient@v0.3.0?min";
 export * from "https://cdn.skypack.dev/numeric-quantity@v1.0.1?min";
 export * as Zod from "https://cdn.skypack.dev/zod@3.0.0-alpha.33?dts";
