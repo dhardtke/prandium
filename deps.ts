762279d74de2ef6a177f597e0470d1df73a7bd20
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-05-14 03:25:22 +0200

    fix: Re-enable lock file pinning, upgrade Zod again

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
 export * as sqlite from "https://deno.land/x/sqlite@v2.4.0/mod.ts";
 export * as Dom from "https://deno.land/x/deno_dom@v0.1.8-alpha/deno-dom-wasm.ts";
 export * as Oak from "https://deno.land/x/oak@v7.4.1/mod.ts";
-export { default as parseIngredient } from "https://cdn.skypack.dev/parse-ingredient@v0.3.0?dts";
-export * from "https://cdn.skypack.dev/numeric-quantity@v1.0.1?dts";
-export * as Zod from "https://cdn.skypack.dev/zod@3.0.0-alpha.33?dts";
+export { default as parseIngredient } from "https://cdn.skypack.dev/pin/parse-ingredient@v0.3.0-oGCvUl895cD38Srdtr7M/mode=imports/optimized/parse-ingredient.js";
+export * from "https://cdn.skypack.dev/pin/numeric-quantity@v1.0.1-goQiL1bZ29fHjB8V1O03/mode=imports/optimized/numeric-quantity.js";
+export { z as Zod } from "https://deno.land/x/zod@3.0.0-alpha.39/mod.ts";
 
 import {
   format,
