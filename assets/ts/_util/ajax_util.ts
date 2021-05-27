b7d4ecc9d597d348bdc5bf557727a9b09daec5bd
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-05-27 20:39:32 +0200

    fix: Ensure cookies are sent with AJAX requests

--- assets/ts/_util/ajax_util.ts
+++ assets/ts/_util/ajax_util.ts
@@ -1,7 +1,9 @@
 import { bootComponents, destroyComponents } from "../components/component.ts";
 
 export async function replaceMain(url: string) {
-  const response = await fetch(url);
+  const response = await fetch(url, {
+    credentials: "same-origin"
+  });
   if (response.status === 200) {
     const $document = new DOMParser().parseFromString(await response.text(), "text/html");
     destroyComponents();
