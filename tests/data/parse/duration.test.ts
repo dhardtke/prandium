5949a970461181f89a17948261933fb257c63cb2
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-06-03 14:15:53 +0200

    test: Improve duration tests

--- tests/data/parse/duration.test.ts
+++ tests/data/parse/duration.test.ts
@@ -1,80 +1,108 @@
-import { Duration, parseDuration } from "../../../src/data/parse/duration.ts";
 import { assertEquals, assertThrows } from "../../../deps.ts";
+import {
+  Duration,
+  durationToSeconds,
+  parseDuration,
+} from "../../../src/data/parse/duration.ts";
 
 Deno.test(`Parse duration datetime successful`, () => {
   const tests = new Map<string, Duration>([
-    ["P0003-06-04T12:30:05", {
-      years: 3,
-      months: 6,
-      weeks: 0,
-      days: 4,
-      hours: 12,
-      minutes: 30,
-      seconds: 5,
-    }],
-    ["PT0S", {
-      years: 0,
-      months: 0,
-      weeks: 0,
-      days: 0,
-      hours: 0,
-      minutes: 0,
-      seconds: 0,
-    }],
-    ["P0D", {
-      years: 0,
-      months: 0,
-      weeks: 0,
-      days: 0,
-      hours: 0,
-      minutes: 0,
-      seconds: 0,
-    }],
-    ["PT36H", {
-      years: 0,
-      months: 0,
-      weeks: 0,
-      days: 0,
-      hours: 36,
-      minutes: 0,
-      seconds: 0,
-    }],
-    ["P1DT12H", {
-      years: 0,
-      months: 0,
-      weeks: 0,
-      days: 1,
-      hours: 12,
-      minutes: 0,
-      seconds: 0,
-    }],
-    ["P0.5Y", {
-      years: 0.5,
-      months: 0,
-      weeks: 0,
-      days: 0,
-      hours: 0,
-      minutes: 0,
-      seconds: 0,
-    }],
-    ["PT0.5S", {
-      years: 0,
-      months: 0,
-      weeks: 0,
-      days: 0,
-      hours: 0,
-      minutes: 0,
-      seconds: 0.5,
-    }],
-    ["PT0,5S", {
-      years: 0,
-      months: 0,
-      weeks: 0,
-      days: 0,
-      hours: 0,
-      minutes: 0,
-      seconds: 0.5,
-    }],
+    [
+      "P0003-06-04T12:30:05",
+      {
+        years: 3,
+        months: 6,
+        weeks: 0,
+        days: 4,
+        hours: 12,
+        minutes: 30,
+        seconds: 5,
+      },
+    ],
+    [
+      "PT0S",
+      {
+        years: 0,
+        months: 0,
+        weeks: 0,
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      },
+    ],
+    [
+      "P0D",
+      {
+        years: 0,
+        months: 0,
+        weeks: 0,
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      },
+    ],
+    [
+      "PT36H",
+      {
+        years: 0,
+        months: 0,
+        weeks: 0,
+        days: 0,
+        hours: 36,
+        minutes: 0,
+        seconds: 0,
+      },
+    ],
+    [
+      "P1DT12H",
+      {
+        years: 0,
+        months: 0,
+        weeks: 0,
+        days: 1,
+        hours: 12,
+        minutes: 0,
+        seconds: 0,
+      },
+    ],
+    [
+      "P0.5Y",
+      {
+        years: 0.5,
+        months: 0,
+        weeks: 0,
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      },
+    ],
+    [
+      "PT0.5S",
+      {
+        years: 0,
+        months: 0,
+        weeks: 0,
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0.5,
+      },
+    ],
+    [
+      "PT0,5S",
+      {
+        years: 0,
+        months: 0,
+        weeks: 0,
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0.5,
+      },
+    ],
   ]);
   for (const [input, expected] of tests.entries()) {
     const actual = parseDuration(input);
@@ -118,4 +146,115 @@ Deno.test(`Parse duration unsuccessful`, () => {
       `parseDuration('${input}') should throw`,
     );
   }
+});
+
+Deno.test(`durationToSeconds`, () => {
+  assertEquals(
+    durationToSeconds({
+      years: 0,
+      months: 0,
+      weeks: 0,
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    }),
+    0,
+  );
+  assertEquals(
+    durationToSeconds({
+      years: 0,
+      months: 0,
+      weeks: 0,
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 1,
+    }),
+    1,
+  );
+  assertEquals(
+    durationToSeconds({
+      years: 0,
+      months: 0,
+      weeks: 0,
+      days: 0,
+      hours: 0,
+      minutes: 1,
+      seconds: 0,
+    }),
+    60,
+  );
+  assertEquals(
+    durationToSeconds({
+      years: 0,
+      months: 0,
+      weeks: 0,
+      days: 0,
+      hours: 1,
+      minutes: 0,
+      seconds: 0,
+    }),
+    60 * 60,
+  );
+  assertEquals(
+    durationToSeconds({
+      years: 0,
+      months: 0,
+      weeks: 0,
+      days: 1,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    }),
+    24 * 60 * 60,
+  );
+  assertEquals(
+    durationToSeconds({
+      years: 0,
+      months: 0,
+      weeks: 1,
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    }),
+    7 * 24 * 60 * 60,
+  );
+  assertEquals(
+    durationToSeconds({
+      years: 0,
+      months: 1,
+      weeks: 0,
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    }),
+    31 * 24 * 60 * 60,
+  );
+  assertEquals(
+    durationToSeconds({
+      years: 1,
+      months: 0,
+      weeks: 0,
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    }),
+    365 * 24 * 60 * 60,
+  );
+  assertEquals(
+    durationToSeconds({
+      years: 3,
+      months: 2,
+      weeks: 4,
+      days: 5,
+      hours: 10,
+      minutes: 47,
+      seconds: 36,
+    }),
+    (((365 * 3 + 2 * 31 + 4 * 7 + 5) * 24 + 10) * 60 + 47) * 60 + 36,
+  );
 });
