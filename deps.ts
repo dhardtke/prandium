23a662ba390fda7fe380f2e33d055c5449d0f7a9
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-06-23 21:21:18 +0200

    feat: Switch to esm.sh to re-enable lockfile pinnig

--- deps.ts
+++ deps.ts
@@ -1,18 +1,20 @@
+// @formatter:off
 export * as sqlite from "https://deno.land/x/sqlite@v2.4.2/mod.ts";
 export * as Dom from "https://deno.land/x/deno_dom@v0.1.12-alpha/deno-dom-wasm.ts";
 export * as Oak from "https://deno.land/x/oak@v7.6.3/mod.ts";
-export { default as parseIngredient } from "https://cdn.skypack.dev/parse-ingredient@v0.3.0?min";
-export * from "https://cdn.skypack.dev/numeric-quantity@v1.0.1?min";
 export { z as Zod } from "https://deno.land/x/zod@v3.2/mod.ts";
+
+export { default as parseIngredient } from "https://esm.sh/parse-ingredient@0.3.0";
+export * from "https://esm.sh/numeric-quantity@1.0.1";
 
 import {
   format,
   formatDuration,
   intervalToDuration,
   formatDistanceToNow
-} from "https://cdn.skypack.dev/date-fns@v2.22.1?min";
-import { default as en } from "https://cdn.skypack.dev/date-fns@v2.22.1/locale/en-US?min";
-import { default as de } from "https://cdn.skypack.dev/date-fns@v2.22.1/locale/de?min";
+} from "https://esm.sh/date-fns@2.22.1?no-check";
+import { default as en } from "https://esm.sh/date-fns@2.22.1/locale/en-US?no-check";
+import { default as de } from "https://esm.sh/date-fns@2.22.1/locale/de?no-check";
 
 export const dateFns = {
   format,
@@ -40,7 +42,7 @@ export type {
   NutritionInformation as SchemaNutritionInformation,
   Recipe as SchemaRecipe,
   Review as SchemaReview,
-} from "https://cdn.skypack.dev/schema-dts@v0.8.3/schema.d.ts?min";
+} from "https://esm.sh/schema-dts@0.8.3";
 
 // tests
 export {
@@ -50,4 +52,5 @@ export {
   unreachable,
 } from "https://deno.land/std@0.99.0/testing/asserts.ts";
 
-export {test, TestSuite } from "https://deno.land/x/test_suite@v0.7.0/mod.ts";
+export { test, TestSuite } from "https://deno.land/x/test_suite@v0.7.0/mod.ts";
+// @formatter:on
