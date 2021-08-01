5799c3f9a91a5347ad4f5719a287a01122f04620
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-08-01 22:56:50 +0200

    fix(perf): Add missing foreign key indexes

---
+++ src/data/migrations/3_foreign_key_indexes.ts
@@ -0,0 +1,21 @@
+import { Database } from "../db.ts";
+import { Migration } from "./migration.ts";
+
+export const ForeignKeyIndexes = new class InitialMigration extends Migration {
+  constructor() {
+    super(3);
+  }
+
+  migrate(db: Database) {
+    const queries = [
+      `CREATE INDEX recipe_tag_tag_id_recipe_id ON recipe_tag (tag_id, recipe_id)`,
+      `CREATE INDEX recipe_tag_tag_id ON recipe_tag (tag_id)`,
+      `CREATE INDEX recipe_tag_recipe_id ON recipe_tag (recipe_id)`,
+      `CREATE INDEX recipe_history_recipe_id ON recipe_history (recipe_id)`,
+      `CREATE INDEX recipe_review_recipe_id ON recipe_review (recipe_id)`,
+    ];
+    for (const sql of queries) {
+      db.exec(sql);
+    }
+  }
+}();
--- src/data/migrations/mod.ts
+++ src/data/migrations/mod.ts
@@ -1,8 +1,10 @@
 import { InitialMigration } from "./1_initial_migration.ts";
 import { AddFlagColumn } from "./2_add_flag_column.ts";
+import { ForeignKeyIndexes } from "./3_foreign_key_indexes.ts";
 import { Migration } from "./migration.ts";
 
 export const MIGRATIONS: Migration[] = [
   InitialMigration,
   AddFlagColumn,
+  ForeignKeyIndexes,
 ];
