f5a7ada8468792b2360b7d81da1f9a848d173178
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-08-03 23:05:50 +0200

    feat(deps): Update dependencies

--- deps.ts
+++ deps.ts
@@ -2,7 +2,7 @@
 export * as sqlite from "https://deno.land/x/sqlite@v3.0.0/mod.ts";
 export * as Dom from "https://deno.land/x/deno_dom@v0.1.12-alpha/deno-dom-wasm.ts";
 export * as Oak from "https://deno.land/x/oak@v8.0.0/mod.ts";
-export { z as Zod } from "https://deno.land/x/zod@v3.2/mod.ts";
+export { z as Zod } from "https://deno.land/x/zod@v3.7.0/mod.ts";
 
 export { default as parseIngredient } from "https://esm.sh/parse-ingredient@0.3.0";
 export * from "https://esm.sh/numeric-quantity@1.0.1";
@@ -12,9 +12,9 @@ import {
   formatDuration,
   intervalToDuration,
   formatDistanceToNow
-} from "https://esm.sh/date-fns@2.22.1?no-check";
-import { default as en } from "https://esm.sh/date-fns@2.22.1/locale/en-US?no-check";
-import { default as de } from "https://esm.sh/date-fns@2.22.1/locale/de?no-check";
+} from "https://esm.sh/date-fns@2.23.0?no-check";
+import { default as en } from "https://esm.sh/date-fns@2.23.0/locale/en-US?no-check";
+import { default as de } from "https://esm.sh/date-fns@2.23.0/locale/de?no-check";
 
 export const dateFns = {
   format,
@@ -31,7 +31,7 @@ export const dateFns = {
 export * as path from "https://deno.land/std@0.103.0/path/mod.ts";
 export * as log from "https://deno.land/std@0.103.0/log/mod.ts";
 export { LogRecord } from "https://deno.land/std@0.103.0/log/logger.ts";
-export * as Cliffy from "https://deno.land/x/cliffy@v0.19.1/command/mod.ts";
+export * as Cliffy from "https://deno.land/x/cliffy@v0.19.4/command/mod.ts";
 export * as fs from "https://deno.land/std@0.103.0/fs/mod.ts";
 export * as Colors from "https://deno.land/std@0.103.0/fmt/colors.ts";
 export { default as slash } from "https://deno.land/x/slash@v0.3.0/mod.ts";
@@ -42,7 +42,7 @@ export type {
   NutritionInformation as SchemaNutritionInformation,
   Recipe as SchemaRecipe,
   Review as SchemaReview,
-} from "https://esm.sh/schema-dts@0.8.3";
+} from "https://esm.sh/schema-dts@0.10.0";
 
 // tests
 export {
@@ -52,5 +52,5 @@ export {
   unreachable,
 } from "https://deno.land/std@0.103.0/testing/asserts.ts";
 
-export { test, TestSuite } from "https://deno.land/x/test_suite@v0.7.0/mod.ts";
+export { test, TestSuite } from "https://deno.land/x/test_suite@v0.8.0/mod.ts";
 // @formatter:on
