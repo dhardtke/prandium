cf944b64b38b2fad9973be2e348879b19087b20c
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-08-25 22:40:37 +0200

    fix(dev): Fix build.ts on Windows

--- deps.ts
+++ deps.ts
@@ -1,15 +1,15 @@
 // @formatter:off
-export * as sqlite from "https://deno.land/x/sqlite@v3.1.0/mod.ts";
-export * as Oak from "https://deno.land/x/oak@v8.0.0/mod.ts";
-export { z as Zod } from "https://deno.land/x/zod@v3.7.0/mod.ts";
+export * as sqlite from "https://deno.land/x/sqlite@v3.1.1/mod.ts";
+export * as Oak from "https://deno.land/x/oak@v9.0.0/mod.ts";
+export { z as Zod } from "https://deno.land/x/zod@v3.8.0/mod.ts";
 
 // utils
-export * as path from "https://deno.land/std@0.105.0/path/mod.ts";
-export * as log from "https://deno.land/std@0.105.0/log/mod.ts";
-export { LogRecord } from "https://deno.land/std@0.105.0/log/logger.ts";
-export * as fs from "https://deno.land/std@0.105.0/fs/mod.ts";
-export * as Colors from "https://deno.land/std@0.105.0/fmt/colors.ts";
-export { sprintf } from "https://deno.land/std@0.105.0/fmt/printf.ts";
+export * as path from "https://deno.land/std@0.106.0/path/mod.ts";
+export * as log from "https://deno.land/std@0.106.0/log/mod.ts";
+export { LogRecord } from "https://deno.land/std@0.106.0/log/logger.ts";
+export * as fs from "https://deno.land/std@0.106.0/fs/mod.ts";
+export * as Colors from "https://deno.land/std@0.106.0/fmt/colors.ts";
+export { sprintf } from "https://deno.land/std@0.106.0/fmt/printf.ts";
 export { default as slash } from "https://deno.land/x/slash@v0.3.0/mod.ts";
 
 // types
@@ -18,7 +18,7 @@ export type {
   NutritionInformation as SchemaNutritionInformation,
   Recipe as SchemaRecipe,
   Review as SchemaReview,
-} from "https://esm.sh/schema-dts@0.10.0";
+} from "https://esm.sh/schema-dts@0.11.0";
 
 // tests
 export {
@@ -26,7 +26,7 @@ export {
   assertNotEquals,
   assertThrows,
   unreachable,
-} from "https://deno.land/std@0.105.0/testing/asserts.ts";
+} from "https://deno.land/std@0.106.0/testing/asserts.ts";
 
 export { test, TestSuite } from "https://deno.land/x/test_suite@v0.8.0/mod.ts";
 // @formatter:on
