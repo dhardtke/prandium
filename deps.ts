631b320dbb96a2e7f737332855539467eece6483
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-10-01 22:00:54 +0200

    feat(assets): Update to latest Deno and std

--- deps.ts
+++ deps.ts
@@ -3,12 +3,12 @@ export * as sqlite from "https://deno.land/x/sqlite@v3.1.1/mod.ts";
 export * as Oak from "https://deno.land/x/oak@v9.0.0/mod.ts";
 
 // utils
-export * as path from "https://deno.land/std@0.106.0/path/mod.ts";
-export * as log from "https://deno.land/std@0.106.0/log/mod.ts";
-export { LogRecord } from "https://deno.land/std@0.106.0/log/logger.ts";
-export * as fs from "https://deno.land/std@0.106.0/fs/mod.ts";
-export * as Colors from "https://deno.land/std@0.106.0/fmt/colors.ts";
-export { sprintf } from "https://deno.land/std@0.106.0/fmt/printf.ts";
+export * as path from "https://deno.land/std@0.107.0/path/mod.ts";
+export * as log from "https://deno.land/std@0.107.0/log/mod.ts";
+export { LogRecord } from "https://deno.land/std@0.107.0/log/logger.ts";
+export * as fs from "https://deno.land/std@0.107.0/fs/mod.ts";
+export * as Colors from "https://deno.land/std@0.107.0/fmt/colors.ts";
+export { sprintf } from "https://deno.land/std@0.107.0/fmt/printf.ts";
 export { default as slash } from "https://deno.land/x/slash@v0.3.0/mod.ts";
 
 // types
@@ -26,7 +26,7 @@ export {
   assertThrows,
   assertThrowsAsync,
   unreachable,
-} from "https://deno.land/std@0.106.0/testing/asserts.ts";
+} from "https://deno.land/std@0.107.0/testing/asserts.ts";
 
 export { test, TestSuite } from "https://deno.land/x/test_suite@v0.8.0/mod.ts";
 // @formatter:on
