43a8d0db921c68fc26cdc2fae48a141526f7b273
dhardtke <1360135+dhardtke@users.noreply.github.com>
2021-10-01 21:50:44 +0200

    feat(assets): Begin work on custom FE implementation

--- assets/deps.ts
+++ assets/deps.ts
@@ -1,5 +1,5 @@
-import * as Collapse from "https://cdn.skypack.dev/bootstrap@5.1.0/js/src/collapse.js";
-import * as Dropdown from "https://cdn.skypack.dev/bootstrap@5.1.0/js/src/dropdown.js";
-
-console.assert(Boolean(Collapse));
-console.assert(Boolean(Dropdown));
+// import * as Collapse from "https://cdn.skypack.dev/bootstrap@5.1.0/js/src/collapse.js";
+// import * as Dropdown from "https://cdn.skypack.dev/bootstrap@5.1.0/js/src/dropdown.js";
+//
+// console.assert(Boolean(Collapse));
+// console.assert(Boolean(Dropdown));
