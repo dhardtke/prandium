5b29943b30a7015896a85399804ae85011a3942a
dhardtke <1360135+dhardtke@users.noreply.github.com>
2022-02-13 16:31:10 +0100

    refactor: Remove unneeded ts file

--- assets/deps.ts
+++
@@ -1,5 +0,0 @@
-// import * as Collapse from "https://cdn.skypack.dev/bootstrap@5.1.0/js/src/collapse.js";
-// import * as Dropdown from "https://cdn.skypack.dev/bootstrap@5.1.0/js/src/dropdown.js";
-//
-// console.assert(Boolean(Collapse));
-// console.assert(Boolean(Dropdown));
--- assets/index.ts
+++ assets/index.ts
@@ -3,7 +3,6 @@
 /// <reference lib="dom.iterable" />
 /// <reference lib="deno.ns" />
 
-import "./deps.ts";
 import { bootComponents } from "./ts/components/component.ts";
 import { Observer } from "./ts/components/observer.ts";
 import { NavbarDarkModeSwitcher } from "./ts/global/_navbar_dark_mode_switcher.ts";
