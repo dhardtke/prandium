a6ebb541329bf7d699d545ea35c09751b69e5de9
dhardtke <1360135+dhardtke@users.noreply.github.com>
2022-02-19 15:12:44 +0100

    fix: Fix build error

--- tests/data/util/thumbnails.test.ts
+++ tests/data/util/thumbnails.test.ts
@@ -3,9 +3,9 @@ import { downloadThumbnail } from "../../../src/data/util/thumbnails.ts";
 import { withTemp } from "../../_internal/with-temp.function.ts";
 
 Deno.test(`downloadThumbnail should use UTF8 filenames`, async () => {
-  const mockedFetchFn = () =>
+  const mockedFetchFn = (_input: RequestInfo, _userAgent: string, _init?: RequestInit): Promise<Response> =>
     Promise.resolve(
-      { arrayBuffer: () => new ArrayBuffer(0) },
+      { arrayBuffer: () => new ArrayBuffer(0) } as unknown as Response,
     );
 
   await withTemp(async (tmpDir) => {
