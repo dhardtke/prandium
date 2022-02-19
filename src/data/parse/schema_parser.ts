d211922e8fd6b658c696348e8916865f744b2e5a
dhardtke <1360135+dhardtke@users.noreply.github.com>
2022-02-19 13:46:09 +0100

    fix: Improve typing of SchemaParser

--- src/data/parse/schema_parser.ts
+++ src/data/parse/schema_parser.ts
@@ -7,9 +7,7 @@ const ScriptOpen = "<script";
 const AngleBracketClose = ">";
 
 export class SchemaParser {
-  private readonly html: string;
-
-  constructor(html: string) {
+  constructor(private readonly html: string) {
     this.html = html;
   }
 
@@ -20,7 +18,7 @@ export class SchemaParser {
     return this.findFirst<SchemaRecipe>("Recipe");
   }
 
-  private *collectJsons(): Generator<string> {
+  private* collectJsons(): Generator<string> {
     for (
       let pos = this.html.indexOf(ScriptOpen);
       pos !== -1;
@@ -38,7 +36,7 @@ export class SchemaParser {
             openingScriptEnd + AngleBracketClose.length,
           );
           if (
-            ["", "'", '"'].some((char) =>
+            ["", "'", "\""].some((char) =>
               scriptAttributes.includes(`type=${char}${LdJson}${char}`)
             )
           ) {
@@ -60,7 +58,7 @@ export class SchemaParser {
         for (const candidate of candidates) {
           if ("@type" in candidate && candidate["@type"] === type) {
             delete candidate["@context"];
-            return candidate;
+            return candidate as unknown as T;
           }
         }
       } catch (e) {
@@ -72,8 +70,8 @@ export class SchemaParser {
   }
 
   // deno-lint-ignore no-explicit-any
-  private static getObjects(parsed: any): unknown[] {
-    if (Array.isArray(parsed) === "array") {
+  private static getObjects(parsed: any): { [k: string]: unknown }[] {
+    if (Array.isArray(parsed)) {
       return parsed;
     }
     if ("@graph" in parsed && Array.isArray(parsed["@graph"])) {
