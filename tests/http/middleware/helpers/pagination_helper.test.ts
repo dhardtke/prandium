51d640522888dc1575adf13584a345ddb3bd801e
dhardtke <1360135+dhardtke@users.noreply.github.com>
2022-03-05 19:38:03 +0100

    test: Add pagination_helper.test.ts

---
+++ tests/http/middleware/helpers/pagination_helper.test.ts
@@ -0,0 +1,45 @@
+import { assertEquals } from "../../../../deps.ts";
+import { Oak } from "../../../../deps_oak.ts";
+import { type Settings } from "../../../../src/data/settings.ts";
+import { PaginationHelper } from "../../../../src/http/middleware/helpers/pagination_helper.ts";
+
+Deno.test("PaginationHelper", async (t) => {
+  const settings: Settings = {
+    pageSize: 12,
+  } as Settings;
+
+  await t.step("buildPaginationParams should use pageSize from settings if parameter is not set", () => {
+    const ctx = Oak.testing.createMockContext({ method: "GET", path: "/" });
+    const params = new PaginationHelper(settings)
+      .buildPaginationParams(ctx);
+    assertEquals(params.pageSize, settings.pageSize);
+  });
+
+  await t.step("buildPaginationParams should remove the flash parameter from the current URL", () => {
+    const ctx = Oak.testing.createMockContext({ method: "GET", path: "/?flash=delete&id=42" });
+    const params = new PaginationHelper(settings)
+      .buildPaginationParams(ctx);
+    assertEquals(params.currentUrl, "http://localhost/?id=42");
+  });
+
+  await t.step("buildPaginationParams should set page to 1 if parameter is not present", () => {
+    const ctx = Oak.testing.createMockContext({ method: "GET", path: "/" });
+    const params = new PaginationHelper(settings)
+      .buildPaginationParams(ctx);
+    assertEquals(params.page, 1);
+  });
+
+  await t.step("buildPaginationParams should use pageSize from URL if present", () => {
+    const ctx = Oak.testing.createMockContext({ method: "GET", path: "/?pageSize=25" });
+    const params = new PaginationHelper(settings)
+      .buildPaginationParams(ctx);
+    assertEquals(params.pageSize, 25);
+  });
+
+  await t.step("buildPaginationParams should use page from URL if present", () => {
+    const ctx = Oak.testing.createMockContext({ method: "GET", path: "/?page=3" });
+    const params = new PaginationHelper(settings)
+      .buildPaginationParams(ctx);
+    assertEquals(params.page, 3);
+  });
+});
