ffa33242f69702fccc409e3fb92f6fc477498c74
dhardtke <1360135+dhardtke@users.noreply.github.com>
2022-03-05 19:29:57 +0100

    test: Add index.router.test

---
+++ tests/http/router/index.router.test.ts
@@ -0,0 +1,63 @@
+import { assertEquals, container } from "../../../deps.ts";
+import { Oak } from "../../../deps_oak.ts";
+import { IndexController } from "../../../src/controllers/index.controller.ts";
+import { PaginationParams } from "../../../src/data/pagination.ts";
+import { OrderBy } from "../../../src/data/service/util/order-by.ts";
+import { SETTINGS } from "../../../src/di.ts";
+import { DEFAULT_ORDER_BY, IndexRouter } from "../../../src/http/routers/index.router.ts";
+
+Deno.test("IndexRouter", async (t) => {
+  let lastListInvocation: {
+    filters: {
+      tagIds: number[];
+      title: string;
+    };
+    showTagFilter: boolean;
+    orderBy: OrderBy | undefined;
+    paginationParams: PaginationParams;
+  } | undefined = undefined;
+
+  const mockIndexController: Partial<IndexController> = {
+    list(filters: { tagIds: number[]; title: string }, showTagFilter: boolean, orderBy: OrderBy | undefined, paginationParams: PaginationParams) {
+      lastListInvocation = { filters, showTagFilter, orderBy, paginationParams };
+      return "";
+    },
+  };
+  container.register(IndexController, { useValue: mockIndexController });
+
+  const pageSize = 25;
+  container.register(SETTINGS, { useValue: { pageSize } });
+
+  async function invokeRoute(path: string) {
+    const ctx = Oak.testing.createMockContext({ method: "GET", path });
+    const next = Oak.testing.createMockNext();
+    const mw = IndexRouter.routes();
+    await mw(ctx, next);
+  }
+
+  await t.step("when given no parameters, should use default parameters", async () => {
+    await invokeRoute("/");
+
+    assertEquals(lastListInvocation, {
+      filters: { tagIds: [], title: "" },
+      showTagFilter: false,
+      orderBy: DEFAULT_ORDER_BY,
+      paginationParams: { page: 1, pageSize, currentUrl: "http://localhost/" },
+    });
+  });
+
+  await t.step("when given filters, should pass them to the controller", async () => {
+    const path = "/?tagId=1&tagId=2&tagId=3&tagId=4&tagId=5&tagId=6&tagId=9&title=lorem+ipsum";
+    await invokeRoute(path);
+
+    assertEquals(
+      lastListInvocation,
+      {
+        filters: { tagIds: [1, 2, 3, 4, 5, 6, 9], title: "lorem ipsum" },
+        showTagFilter: false,
+        orderBy: DEFAULT_ORDER_BY,
+        paginationParams: { page: 1, pageSize, currentUrl: `http://localhost${path}` },
+      },
+    );
+  });
+});
