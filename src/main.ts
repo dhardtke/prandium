4a644b2b42d70ce14dda185ce09c4f045dad30e8
dhardtke <1360135+dhardtke@users.noreply.github.com>
2022-03-05 15:57:04 +0100

    perf: Mark Database, SETTINGS and CONFIG_DIR as singletons when registering

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Colors, container, fs, log, LogRecord, path } from "../deps.ts";
+import { Colors, container, fs, Lifecycle, log, LogRecord, path } from "../deps.ts";
 import { Database } from "./data/db.ts";
 import { Argparser } from "./data/parse/argparser.ts";
 import { readFromDisk, Settings } from "./data/settings.ts";
@@ -137,9 +137,9 @@ async function main(): Promise<number> {
   // Dependency Injection registration
   const database = new Database(buildDbPath(options.configDir));
   database.migrate();
-  container.register(Database, { useValue: database });
-  container.register(SETTINGS, { useValue: settings });
-  container.register(CONFIG_DIR, { useValue: options.configDir });
+  container.register(Database, { useValue: database }, { lifeCycle: Lifecycle.Singleton });
+  container.register(SETTINGS, { useValue: settings }, { lifeCycle: Lifecycle.Singleton });
+  container.register(CONFIG_DIR, { useValue: options.configDir }, { lifeCycle: Lifecycle.Singleton });
 
   await spawnServer({
     ...options,
