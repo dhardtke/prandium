2e0bf7731fe63d1c53f7598e300512ac5833af60
dhardtke <1360135+dhardtke@users.noreply.github.com>
2022-03-06 02:33:04 +0100

    test: Add thumbnails.router.test, various fixes

--- src/http/routers/thumbnails.router.ts
+++ src/http/routers/thumbnails.router.ts
@@ -1,10 +1,13 @@
 import { Oak } from "../../../deps-oak.ts";
+import { inject, singleton } from "../../../deps.ts";
 import { getThumbnailDir } from "../../data/util/thumbnails.ts";
+import { CONFIG_DIR } from "../../di.ts";
 import { AppState } from "../webserver.ts";
 import { Router } from "./router.ts";
 
+@singleton()
 export class ThumbnailsRouter extends Router {
-  constructor() {
+  constructor(@inject(CONFIG_DIR) private configDir: string) {
     super();
     this.router.get("/thumbnails/(.+)", this.get);
   }
@@ -12,7 +15,7 @@ export class ThumbnailsRouter extends Router {
   get = async (ctx: Oak.RouterContext<"/thumbnails/(.+)", { 0: string }, AppState>, next: () => Promise<unknown>) => {
     try {
       await Oak.send(ctx, ctx.params[0]!, {
-        root: getThumbnailDir(ctx.state.configDir),
+        root: getThumbnailDir(this.configDir),
       });
     } catch {
       await next();
--- tests/_internal/with-temp.function.ts
+++ tests/_internal/with-temp.function.ts
@@ -1,12 +1,15 @@
 export function withTemp(
-  test: (tmpDir: string) => Promise<void>,
+  test: (tmpDir: string) => Promise<void> | void,
+  dontClean?: boolean,
 ): () => Promise<void> {
   return async () => {
     const tmpDir = await Deno.makeTempDir();
     try {
       await test(tmpDir);
     } finally {
-      await Deno.remove(tmpDir, { recursive: true });
+      if (!dontClean) {
+        await Deno.remove(tmpDir, { recursive: true });
+      }
     }
   };
 }
