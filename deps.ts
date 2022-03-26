06b71ddc55f3855a2dffbbb31d05102990529f8e
dhardtke <1360135+dhardtke@users.noreply.github.com>
2022-03-26 20:45:31 +0100

    chore: Update deps

--- deps.ts
+++ deps.ts
@@ -1,16 +1,16 @@
 // @formatter:off
-export * as sqlite from "https://deno.land/x/sqlite@v3.2.1/mod.ts";
-import * as Reflect from "https://esm.sh/@abraham/reflection@0.9.1?pin=v66&no-check";
+export * as sqlite from "https://deno.land/x/sqlite@v3.3.0/mod.ts";
+import * as Reflect from "https://esm.sh/@abraham/reflection@0.10.0?pin=v66&no-check";
 console.assert(Boolean(Reflect));
 export { container, injectable, singleton, inject, Lifecycle, registry } from "https://esm.sh/tsyringe@4.6.0?pin=v66";
 
 // utils
-export * as path from "https://deno.land/std@0.129.0/path/mod.ts";
-export * as log from "https://deno.land/std@0.129.0/log/mod.ts";
-export { LogRecord } from "https://deno.land/std@0.129.0/log/logger.ts";
-export * as fs from "https://deno.land/std@0.129.0/fs/mod.ts";
-export * as Colors from "https://deno.land/std@0.129.0/fmt/colors.ts";
-export { sprintf } from "https://deno.land/std@0.129.0/fmt/printf.ts";
+export * as path from "https://deno.land/std@0.132.0/path/mod.ts";
+export * as log from "https://deno.land/std@0.132.0/log/mod.ts";
+export { LogRecord } from "https://deno.land/std@0.132.0/log/logger.ts";
+export * as fs from "https://deno.land/std@0.132.0/fs/mod.ts";
+export * as Colors from "https://deno.land/std@0.132.0/fmt/colors.ts";
+export { sprintf } from "https://deno.land/std@0.132.0/fmt/printf.ts";
 export { default as slash } from "https://deno.land/x/slash@v0.3.0/mod.ts";
 
 // types
@@ -19,7 +19,7 @@ export type {
   NutritionInformation as SchemaNutritionInformation,
   Recipe as SchemaRecipe,
   Review as SchemaReview,
-} from "https://esm.sh/schema-dts@1.0.0?pin=v66";
+} from "https://esm.sh/schema-dts@1.1.0?pin=v66";
 
 // tests
 export {
@@ -28,5 +28,5 @@ export {
   assertThrows,
   assertRejects,
   unreachable,
-} from "https://deno.land/std@0.129.0/testing/asserts.ts";
+} from "https://deno.land/std@0.132.0/testing/asserts.ts";
 // @formatter:on
