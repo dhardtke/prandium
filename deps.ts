5a1e5e33f4cc43fe6a07cae1124d0ac17e7dbf7e
Dominik Hardtke <1360135+dhardtke@users.noreply.github.com>
2024-10-17 14:48:52 +0200

    build: Add workaround for missing dependency when building in CI

--- deps.ts
+++ deps.ts
@@ -1,6 +1,9 @@
 // @formatter:off
 export * as sqlite from "https://deno.land/x/sqlite@v3.9.1/mod.ts";
 export * as Oak from "https://deno.land/x/oak@v17.1.0/mod.ts";
+// workaround for https://github.com/dhardtke/prandium/actions/runs/11377150775/job/31650809264
+import oakBuildWorkaround from "npm:path-to-regexp@6.2.1";
+console.assert(!!oakBuildWorkaround);
 export * as needle from "jsr:@needle-di/core";
 import { Container } from "jsr:@needle-di/core";
 export const container = new Container();
